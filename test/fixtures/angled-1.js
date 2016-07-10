9c08ccb485eb97c1d4cae24c0c4bbddbfeb0e5d7
ovidiu <ovidiu.bute@gmail.com>
2016-07-10 12:33:48 +0300

    Add angled data fixture.

---
+++ test/fixtures/angled-1.js
@@ -0,0 +1,390 @@
+module.exports = {
+	language: 'en',
+	textAngle: 1.5999999999999628,
+	orientation: 'Up',
+	regions: [{
+		boundingBox: '138,32,1001,79',
+		lines: [{
+			boundingBox: '138,32,1001,79',
+			words: [{
+				boundingBox: '138,49,360,62',
+				text: 'PROFESSIONALS'
+			}, {
+				boundingBox: '510,44,108,47',
+				text: 'WHO'
+			}, {
+				boundingBox: '637,38,123,50',
+				text: 'CARE'
+			}, {
+				boundingBox: '869,39,123,36',
+				text: 'RandMÄ'
+			}, {
+				boundingBox: '1073,32,66,33',
+				text: '2031'
+			}]
+		}]
+	}, {
+		boundingBox: '1273,7,322,47',
+		lines: [{
+			boundingBox: '1273,7,322,47',
+			words: [{
+				boundingBox: '1273,17,158,37',
+				text: 'Nortlbridge'
+			}, {
+				boundingBox: '1442,11,68,34',
+				text: 'NSW'
+			}, {
+				boundingBox: '1520,7,75,34',
+				text: '2063'
+			}]
+		}]
+	}, {
+		boundingBox: '1669,0,239,119',
+		lines: [{
+			boundingBox: '1702,0,199,35',
+			words: [{
+				boundingBox: '1702,3,71,32',
+				text: 'West'
+			}, {
+				boundingBox: '1783,0,118,32',
+				text: 'LindtEd'
+			}]
+		}, {
+			boundingBox: '1669,78,239,41',
+			words: [{
+				boundingBox: '1669,78,239,41',
+				text: 'www„vetmel'
+			}]
+		}]
+	}, {
+		boundingBox: '621,189,878,74',
+		lines: [{
+			boundingBox: '621,189,878,74',
+			words: [{
+				boundingBox: '621,210,306,53',
+				text: 'Vaccination'
+			}, {
+				boundingBox: '946,208,39,43',
+				text: '&'
+			}, {
+				boundingBox: '1005,198,177,50',
+				text: 'Health'
+			}, {
+				boundingBox: '1201,189,298,53',
+				text: 'Certificate'
+			}]
+		}]
+	}, {
+		boundingBox: '202,423,172,260',
+		lines: [{
+			boundingBox: '202,423,119,26',
+			words: [{
+				boundingBox: '202,424,77,25',
+				text: 'Client'
+			}, {
+				boundingBox: '285,423,36,24',
+				text: 'ID:'
+			}]
+		}, {
+			boundingBox: '205,459,169,25',
+			words: [{
+				boundingBox: '205,459,76,25',
+				text: 'Client'
+			}, {
+				boundingBox: '288,459,86,23',
+				text: 'Name:'
+			}]
+		}, {
+			boundingBox: '219,551,112,24',
+			words: [{
+				boundingBox: '219,551,112,24',
+				text: 'Address:'
+			}]
+		}, {
+			boundingBox: '217,653,144,30',
+			words: [{
+				boundingBox: '217,653,144,30',
+				text: 'Telephone.'
+			}]
+		}]
+	}, {
+		boundingBox: '451,400,361,328',
+		lines: [{
+			boundingBox: '451,400,102,26',
+			words: [{
+				boundingBox: '451,400,102,26',
+				text: '214553'
+			}]
+		}, {
+			boundingBox: '454,433,251,30',
+			words: [{
+				boundingBox: '454,438,33,23',
+				text: 'Dr'
+			}, {
+				boundingBox: '494,437,100,26',
+				text: 'Herron,'
+			}, {
+				boundingBox: '602,433,103,25',
+				text: 'Andrew'
+			}]
+		}, {
+			boundingBox: '459,528,156,32',
+			words: [{
+				boundingBox: '459,530,17,24',
+				text: '7'
+			}, {
+				boundingBox: '484,528,93,32',
+				text: 'Carlyle'
+			}, {
+				boundingBox: '586,528,29,24',
+				text: 'St'
+			}]
+		}, {
+			boundingBox: '461,560,351,31',
+			words: [{
+				boundingBox: '461,561,200,30',
+				text: 'Wollstonecraft,'
+			}, {
+				boundingBox: '671,562,63,24',
+				text: 'NSW'
+			}, {
+				boundingBox: '743,560,69,25',
+				text: '2065'
+			}]
+		}, {
+			boundingBox: '466,650,143,25',
+			words: [{
+				boundingBox: '466,651,67,24',
+				text: '9460'
+			}, {
+				boundingBox: '542,650,67,24',
+				text: '9943'
+			}]
+		}, {
+			boundingBox: '598,687,21,41',
+			words: [{
+				boundingBox: '598,687,21,41',
+				text: '1'
+			}]
+		}]
+	}, {
+		boundingBox: '938,272,404,490',
+		lines: [{
+			boundingBox: '938,272,222,41',
+			words: [{
+				boundingBox: '938,272,222,41',
+				text: '23-05-2016'
+			}]
+		}, {
+			boundingBox: '1183,380,152,29',
+			words: [{
+				boundingBox: '1183,381,103,28',
+				text: 'Patient'
+			}, {
+				boundingBox: '1296,380,39,26',
+				text: 'ID:'
+			}]
+		}, {
+			boundingBox: '1182,421,94,25',
+			words: [{
+				boundingBox: '1182,421,94,25',
+				text: 'Name:'
+			}]
+		}, {
+			boundingBox: '1183,458,120,35',
+			words: [{
+				boundingBox: '1183,458,120,35',
+				text: 'Species:'
+			}]
+		}, {
+			boundingBox: '1184,535,94,26',
+			words: [{
+				boundingBox: '1184,535,94,26',
+				text: 'Breed:'
+			}]
+		}, {
+			boundingBox: '1185,599,60,25',
+			words: [{
+				boundingBox: '1185,599,60,25',
+				text: 'Sex:'
+			}]
+		}, {
+			boundingBox: '1185,645,84,25',
+			words: [{
+				boundingBox: '1185,645,84,25',
+				text: 'Color:'
+			}]
+		}, {
+			boundingBox: '1186,688,147,33',
+			words: [{
+				boundingBox: '1186,688,147,33',
+				text: 'Microchip:'
+			}]
+		}, {
+			boundingBox: '1187,738,155,24',
+			words: [{
+				boundingBox: '1187,738,67,24',
+				text: 'Birth'
+			}, {
+				boundingBox: '1266,738,76,24',
+				text: 'Date:'
+			}]
+		}]
+	}, {
+		boundingBox: '1484,372,311,391',
+		lines: [{
+			boundingBox: '1487,372,118,28',
+			words: [{
+				boundingBox: '1487,372,118,28',
+				text: '131657'
+			}]
+		}, {
+			boundingBox: '1487,411,86,28',
+			words: [{
+				boundingBox: '1487,411,86,28',
+				text: 'Eddie'
+			}]
+		}, {
+			boundingBox: '1486,452,92,27',
+			words: [{
+				boundingBox: '1486,452,92,27',
+				text: 'Feline'
+			}]
+		}, {
+			boundingBox: '1486,525,309,35',
+			words: [{
+				boundingBox: '1486,530,149,30',
+				text: 'Shorthair,'
+			}, {
+				boundingBox: '1647,525,148,30',
+				text: 'Domestic'
+			}]
+		}, {
+			boundingBox: '1486,594,70,27',
+			words: [{
+				boundingBox: '1486,594,70,27',
+				text: 'Male'
+			}]
+		}, {
+			boundingBox: '1485,643,82,25',
+			words: [{
+				boundingBox: '1485,643,82,25',
+				text: 'Black'
+			}]
+		}, {
+			boundingBox: '1485,688,291,26',
+			words: [{
+				boundingBox: '1485,688,291,26',
+				text: '981000300550586'
+			}]
+		}, {
+			boundingBox: '1484,738,175,25',
+			words: [{
+				boundingBox: '1484,738,175,25',
+				text: '01-11-2011'
+			}]
+		}]
+	}, {
+		boundingBox: '232,864,1643,138',
+		lines: [{
+			boundingBox: '232,864,1643,42',
+			words: [{
+				boundingBox: '232,865,7,22',
+				text: 'I'
+			}, {
+				boundingBox: '246,864,89,30',
+				text: 'hereby'
+			}, {
+				boundingBox: '344,864,80,31',
+				text: 'certify'
+			}, {
+				boundingBox: '432,865,55,23',
+				text: 'that'
+			}, {
+				boundingBox: '493,865,43,23',
+				text: 'the'
+			}, {
+				boundingBox: '545,865,79,24',
+				text: 'above'
+			}, {
+				boundingBox: '633,866,88,23',
+				text: 'animal'
+			}, {
+				boundingBox: '731,865,46,24',
+				text: 'has'
+			}, {
+				boundingBox: '786,866,50,24',
+				text: 'had'
+			}, {
+				boundingBox: '845,871,15,19',
+				text: 'a'
+			}, {
+				boundingBox: '870,867,102,30',
+				text: 'general'
+			}, {
+				boundingBox: '983,867,86,25',
+				text: 'health'
+			}, {
+				boundingBox: '1078,868,81,25',
+				text: 'check'
+			}, {
+				boundingBox: '1170,870,51,24',
+				text: 'and'
+			}, {
+				boundingBox: '1231,870,173,26',
+				text: 'vaccination,'
+			}, {
+				boundingBox: '1414,872,55,25',
+				text: 'The'
+			}, {
+				boundingBox: '1479,872,136,34',
+				text: 'following'
+			}, {
+				boundingBox: '1625,875,191,26',
+				text: 'vaccinations'
+			}, {
+				boundingBox: '1826,881,49,20',
+				text: 'are'
+			}]
+		}, {
+			boundingBox: '234,903,99,22',
+			words: [{
+				boundingBox: '234,903,99,22',
+				text: 'current.'
+			}]
+		}, {
+			boundingBox: '240,976,259,26',
+			words: [{
+				boundingBox: '240,976,76,25',
+				text: 'Points'
+			}, {
+				boundingBox: '323,978,25,23',
+				text: 'to'
+			}, {
+				boundingBox: '358,979,141,23',
+				text: 'remember:'
+			}]
+		}]
+	}, {
+		boundingBox: '244,1051,57,23',
+		lines: [{
+			boundingBox: '244,1051,57,23',
+			words: [{
+				boundingBox: '244,1051,57,23',
+				text: '*Full'
+			}]
+		}]
+	}, {
+		boundingBox: '447,1055,216,25',
+		lines: [{
+			boundingBox: '447,1055,216,25',
+			words: [{
+				boundingBox: '447,1055,60,23',
+				text: 'after'
+			}, {
+				boundingBox: '514,1056,149,24',
+				text: 'vaccination'
+			}]
+		}]
+	}]
+}
